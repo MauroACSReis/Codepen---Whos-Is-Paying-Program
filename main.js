53cf117208599bde9222b628cede45d7dc3c742f
Mauro Reis <mauroacsreis@gmail.com>
2022-06-19 16:41:35 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Bootcamp Solution
+
+function whosPaying(names) {
+names=["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+var randomPerson = names[Math.floor(Math.random()*names.length)];
+return randomPerson + " is going to buy lunch today!" ; 
+
+}
